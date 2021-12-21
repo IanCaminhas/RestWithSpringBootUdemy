5827a8abae61ddb01e31034ed65af28a574724c8
IanPC <iancaminhas25@hotmail.com>
2021-12-21 09:26:47 -0300

    CRUD with pacth verb

---
+++ 13 - CRUD with patch verb/src/main/java/br/com/erudio/data/model/Book.java
@@ -0,0 +1,107 @@
+package br.com.erudio.data.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
+
+@Entity
+@Table(name = "books")
+public class Book extends ResourceSupport implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "author", nullable = false, length = 180)
+	private String author;
+	@Column(name = "launch_date", nullable = false, length = 80)
+	@Temporal(TemporalType.DATE)
+	private Date launchDate;
+
+	@Column(nullable = false)
+	private Double price;
+
+	@Column(nullable = false, length = 200)
+	private String title;
+
+	public Book() {
+	}
+
+	@Override
+	public Link getId() {
+		return super.getId();
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getLaunchDate() {
+		return launchDate;
+	}
+
+	public void setLaunchDate(Date launchDate) {
+		this.launchDate = launchDate;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(author, id, launchDate, price, title);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(author, other.author) && Objects.equals(id, other.id)
+				&& Objects.equals(launchDate, other.launchDate) && Objects.equals(price, other.price)
+				&& Objects.equals(title, other.title);
+	}
+
+}
